a14f4ffa40349a4f5e668e9bc1689761223463cd
tam <tami.sargsyan.03@gmail.com>
2024-12-03 23:38:29 +0400

    add hero page mobile version

--- src/pages/Navbar/index.js
+++ src/pages/Navbar/index.js
@@ -3,6 +3,7 @@ import LOGO from "../../assets/logo.svg";
 import "./index.css";
 import FlagDropdown from "../../components/FlagDropdown";
 import Cookies from "js-cookie";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
 const Navbar = () => {
   const menu = [
@@ -31,12 +32,56 @@ const Navbar = () => {
 
   const location = useLocation();
   const lang = Cookies.get("i18next") || "en";
+  const { width } = useWindowSize();
 
   return (
     <div className='navbar container'>
       <NavLink to={`/${lang}/`} className='navbar_logo'>
         <img src={LOGO} alt='Aratta' />
       </NavLink>
+      {width < 750 && (
+        <nav role='navigation'>
+          <div id='menuToggle'>
+            <input type='checkbox' id='menuCheckbox' />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id='menu'>
+              <li>
+                <a href='#'>
+                  <label for='menuCheckbox' onclick='this.parentNode.click();'>
+                    Home
+                  </label>
+                </a>
+              </li>
+              <li>
+                <a href='#about'>
+                  <label for='menuCheckbox' onclick='this.parentNode.click();'>
+                    About
+                  </label>
+                </a>
+              </li>
+
+              <li>
+                <label for='menuCheckbox'>
+                  <a>Info</a>
+                </label>
+              </li>
+              <li>
+                <label for='menuCheckbox'>
+                  <a>Contact</a>
+                </label>
+              </li>
+
+              <li>
+                <a href='https://erikterwan.com/' target='_blank'>
+                  Show me more
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
       <div className='nav_links'>
         {menu.map(m => (
           <NavLink
